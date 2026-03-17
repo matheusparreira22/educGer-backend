de9be27ebd1cf6ea717486d409d0a5131b2676b0
Matheus Parreira <matheusparreira972@gmail.com>
2026-03-17 12:56:33 -0300

    init

---
+++ routes/other/index.js
@@ -0,0 +1,17 @@
+'use strict'
+const object = {
+    name: 'other',
+    age: 20
+}
+
+module.exports = async function (fastify, opts) {
+    fastify.get('/', async function (request, reply) {
+        return object
+    })
+
+    fastify.post('/post', async function (request, reply) {
+        return 'this is an other post'
+    })
+
+
+}
